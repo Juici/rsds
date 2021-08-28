342cc7848b32447928f10d285d9bb3e0a23b3d72
James Whaley <juicy66173@gmail.com>
2021-08-28 13:54:36 +0100

    Add basic header test for TinyFB ROM

--- crates/rom/tests/tiny_fb.rs
+++ crates/rom/tests/tiny_fb.rs
@@ -4,11 +4,11 @@ const TINY_FB: &[u8] = include_bytes!("../../../demo/TinyFB.nds");
 
 #[test]
 fn header() {
-    let header = unsafe { *(TINY_FB as *const [u8] as *const Header) };
+    let rom = TINY_FB;
+    let header = Header::from_reader(rom).unwrap();
 
-    let mut dump = String::new();
-    header.dump(&mut dump).unwrap();
-
-    eprintln!("{}", dump);
-    panic!();
+    assert_eq!(header.game_title, "NDS.TinyFB");
+    assert_eq!(header.game_code, "####");
+    assert_eq!(header.compute_logo_crc16(), 0x9E1A);
+    assert_eq!(header.compute_header_crc16(), 0x908E);
 }
